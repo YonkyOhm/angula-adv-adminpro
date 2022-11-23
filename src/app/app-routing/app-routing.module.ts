a6e6beabaaab3189cfeecce2aee1d05fb020cd54
Raul Calzada <sytherulas@gmail.com>
2022-11-23 13:22:16 -0600

    Se agregaron rutas para la navegacion

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { LoginComponent } from '../auth/login/login.component';
+import { RegisterComponent } from '../auth/register/register.component';
+import { ProgressComponent } from '../pages/progress/progress.component';
+import { Graficas1Component } from '../pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from '../pages/nopagefound/nopagefound.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'progress', component: ProgressComponent},
+  { path: 'grafica1', component: Graficas1Component},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: '**', component: NopagefoundComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot( routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
